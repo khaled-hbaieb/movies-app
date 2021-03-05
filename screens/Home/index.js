8de24b5b21b13b3c92ff8552afd2e7a374bd9be6
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2021-03-05 18:49:04 +0100

    working on local storage

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions, TextInput, ImageBackground} from 'react-native'
+import {Text, View, StyleSheet, Image, TouchableOpacity, ScrollView, Dimensions, TextInput, ImageBackground, Pressable} from 'react-native'
 import Carousel from 'react-native-anchor-carousel'
 import {fontAwesome5, Feather, MaterialIcons} from '@expo/vector-icons'
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
@@ -18,14 +19,13 @@ const Home = () => {
     const [popularMoviesList, setPopularMoviesList] = useState(null)
     const [movieSearch, setMovieSearch] = useState('')
     const [text, setText] = useState('');
-    const [showSearch, setShowSearch] = useState(false)
+    const [dataStorage, setDataStorage] = useState(null)
     const carouselRef = useRef(null)
-    useEffect(() => {
-        const data = axios.get('https://api.themoviedb.org/3/movie/popular?api_key=d684550d631cad69733c812672083206&language=en-US&page=1')
-        .then(result => {
-            setPopularMoviesList(result.data.results)
-        })
-      },[]);
+    
+
+      
+
+
       const renderItem = ({item, index}) => {
     return(
         <View>
@@ -37,16 +37,31 @@ const Home = () => {
             }}>
                 <Image source={{uri: `https://image.tmdb.org/t/p/w185${item.poster_path}`}} style={styles.carouselImage} />
                 <Text style={styles.carouselTitle}>{item.original_title}</Text>
-                <MaterialIcons name='library-add' size={30} color='white' style={styles.carouselIcon}/>
+                <TouchableOpacity style={styles.carouselIcon} onPress={() => addToFav(item)}><MaterialIcons name='library-add' size={30} color='white' /></TouchableOpacity>
             </TouchableOpacity>
+            {movieSearch.length !== 0 && <View style={{width:Dimensions.get('window').width - 14, justifyContent: 'space-between',marginTop: 16}}>
+                <Text style={styles.name}>{item.original_title}</Text>
+                <Text style={styles.stat}>{item.release_date}</Text>
+                <Text style={styles.stat}>{item.vote_average}</Text>
+                
+            </View>}
         </View>
     )
 }
-    
+    const addToFav = async (item) => {
+        // console.log(typeof(JSON.stringify(item.original_title)))
+        try {
+            // await AsyncStorage.setItem(`${item.original_title}`, {item})
+            await AsyncStorage.setItem(JSON.stringify(item.original_title), JSON.stringify({item}))
+          } catch (e) {
+            console.log(e)
+          }
+          
+    }
     const handleSearch = text => {
         
         setText(text)
-        console.log(text)
+        // console.log(text)
         axios.get(`https://api.themoviedb.org/3/search/movie?query=${text}&api_key=d684550d631cad69733c812672083206`)
         .then(result => {
             // console.log(result.data.results,'resuuuuuuuuuuuults')
@@ -58,6 +73,28 @@ const Home = () => {
             setMovieSearch('')
         }
     }
+
+    useEffect(() => {
+        const data = axios.get('https://api.themoviedb.org/3/movie/popular?api_key=d684550d631cad69733c812672083206&language=en-US&page=1')
+        .then(result => {
+            setPopularMoviesList(result.data.results)
+        })
+
+        // const fetchAllItems = async () => {
+        //     try {
+        //         const keys = await AsyncStorage.getAllKeys()
+        //         const items = await AsyncStorage.multiGet(keys)
+        //         console.log(items,'iteeeeeeeeeeeeeeeeeeems')
+        //         return items
+        //     } catch (error) {
+        //         console.log(error, "problemo")
+        //     }
+        // }
+        // fetchAllItems()
+        AsyncStorage.getAllKeys()
+    .then((keys)=> AsyncStorage.multiGet(keys)
+                    .then((data) => console.log(data,'data here')));
+      },[]);
 
     return (
         <ScrollView>
@@ -79,7 +116,7 @@ const Home = () => {
                     <Feather name='search' color='#666' size={22} style={styles.searchBoxIcon} />
                 </View>
                 
-                <Text style={styles.topMoviesHeader}>Top - Movies</Text> 
+                <Text style={styles.topMoviesHeader}>{movieSearch.length === 0 ? 'Top - Movies':`Results for: ${text}`}</Text> 
                 <Carousel style={styles.carousel}
                 data={movieSearch.length === 0 ? popularMoviesList:movieSearch }
                 renderItem={renderItem}
@@ -87,7 +124,9 @@ const Home = () => {
                 ref={carouselRef}
                 containerWidth={width - 20} 
                 separatorWidth={0}
+                inactiveOpacity={0.4}
                 />
+                <Text style={styles.carouselTitle}>Favourites</Text>
                 </ImageBackground>
             </View>
             </View>
@@ -157,6 +196,20 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top:15,
         right:15,
+    },
+    name: {
+        paddingLeft: 14,
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 22,
+        marginBottom: 6,
+    },
+    stat: {
+        paddingLeft: 14,
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 14, 
+        opacity: 0.8
     },
 })
 
